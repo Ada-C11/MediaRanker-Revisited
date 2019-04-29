cea4bd6cbd2ca7a08c6c007bb71834dc6bb91382
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-29 14:23:24 -0700

    updated name column to nickname

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_29_205933) do
+ActiveRecord::Schema.define(version: 2019_04_29_211832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2019_04_29_205933) do
     t.datetime "updated_at", null: false
     t.integer "uid"
     t.string "provider"
-    t.string "name"
+    t.string "nickname"
     t.string "email"
   end
 
