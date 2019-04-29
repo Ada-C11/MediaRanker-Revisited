5d38060525b61a9ebdf9c76b072e4f64cb5e9053
Ariana Bray <aribray@gmail.com>
2019-04-29 16:12:07 -0700

    added test to check for new user creation

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,9 @@
-require "test_helper"
+require 'test_helper'
+require 'pry'
 
 describe UsersController do
-  describe "auth_callback" do
-    it "logs in an existing user and redirects to the root route" do
+  describe 'auth_callback' do
+    it 'logs in an existing user and redirects to the root route' do
       # Count the users, to make sure we're not (for example) creating
       # a new user every time we get a login request
       start_count = User.count
@@ -28,10 +29,25 @@ describe UsersController do
       User.count.must_equal start_count
     end
 
-    it "creates an account for a new user and redirects to the root route" do
+    it 'creates an account for a new user and redirects to the root route' do
+      start_count = User.count
+      user1 = User.new(provider: 'github', uid: 99_999, username: 'camille', email: 'test@user.com')
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user1))
+
+      get github_login_path
+      get auth_callback_path(:github)
+      
+      must_redirect_to root_path
+
+      # Should have created a new user
+      User.count.must_equal start_count + 1
+
+      # The new user's ID should be set in the session
+      session[:user_id].must_equal User.last.id
     end
 
-    it "redirects to the login route if given invalid user data" do
+    it 'redirects to the login route if given invalid user data' do
     end
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,15 +32,15 @@ class ActiveSupport::TestCase
 
   def perform_login(user = nil)
     user ||= User.first
-  
+
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
     get auth_callback_path(:github)
-  
-    return user
+
+    user
   end
 
   def mock_auth_hash(user)
-    return {
+    {
       provider: user.provider,
       uid: user.uid,
       info: {
