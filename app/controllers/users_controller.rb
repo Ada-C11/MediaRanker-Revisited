cee36937d63f38336ec982cd7fb1f1c6b7a693d6
Elise Pham <elise.pham88@gmail.com>
2019-04-29 14:18:55 -0700

    completed basic authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,36 +8,68 @@ class UsersController < ApplicationController
     render_404 unless @user
   end
 
-  def login_form
-  end
+  # def login_form
+  # end
 
-  def login
-    username = params[:username]
-    if username and user = User.find_by(username: username)
-      session[:user_id] = user.id
+  # def login
+  #   username = params[:username]
+  #   if username and user = User.find_by(username: username)
+  #     session[:user_id] = user.id
+  #     flash[:status] = :success
+  #     flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+  #   else
+  #     user = User.new(username: username)
+  #     if user.save
+  #       session[:user_id] = user.id
+  #       flash[:status] = :success
+  #       flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+  #     else
+  #       flash.now[:status] = :failure
+  #       flash.now[:result_text] = "Could not log in"
+  #       flash.now[:messages] = user.errors.messages
+  #       render "login_form", status: :bad_request
+  #       return
+  #     end
+  #   end
+  #   redirect_to root_path
+  # end
+
+  # def logout
+  #   session[:user_id] = nil
+  #   flash[:status] = :success
+  #   flash[:result_text] = "Successfully logged out"
+  #   redirect_to root_path
+  # end
+
+  def create
+    auth_hash = request.env["omniauth.auth"]
+
+    @login_user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if !@login_user.nil?
       flash[:status] = :success
-      flash[:result_text] = "Successfully logged in as existing user #{user.username}"
+      flash[:result_text] = "Successfully logged in as existing user #{@login_user.name}"
     else
-      user = User.new(username: username)
-      if user.save
-        session[:user_id] = user.id
+      @login_user = User.build_from_github(auth_hash)
+
+      if @login_user.save
         flash[:status] = :success
-        flash[:result_text] = "Successfully created new user #{user.username} with ID #{user.id}"
+        flash[:result_text] = "Logged in as new user #{@login_user.name}"
       else
         flash.now[:status] = :failure
-        flash.now[:result_text] = "Could not log in"
-        flash.now[:messages] = user.errors.messages
-        render "login_form", status: :bad_request
-        return
+        flash.now[:result_text] = "Could not create new user account: #{@login_user.errors.messages}"
+        return redirect_to root_path
       end
     end
-    redirect_to root_path
+
+    session[:user_id] = @login_user.id
+    return redirect_to root_path
   end
 
-  def logout
+  def destroy
     session[:user_id] = nil
     flash[:status] = :success
-    flash[:result_text] = "Successfully logged out"
+    flash[:result_text] = "Successfully logged out!"
+
     redirect_to root_path
   end
 end
