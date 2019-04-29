b19dc8e5587cf13aa105524f36d952fcb2bb4bb8
Bitaman <b.amani.s@gmail.com>
2019-04-29 15:12:29 -0700

    fixing the view and added the helper method to user, everything is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,16 +10,29 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.auth"]
+
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user
-      # User was found in the database
       flash[:success] = "Logged in as returning user #{user.name}"
     else
-      # User doesn't match anything in the DB
-      # TODO: Attempt to create a new user
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:success] = "Logged in as new user #{user.name}"
+
+        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
     end
 
     session[:user_id] = user.id
+    return redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
     redirect_to root_path
   end
 
@@ -55,4 +68,5 @@ class UsersController < ApplicationController
   #   flash[:result_text] = "Successfully logged out"
   #   redirect_to root_path
   # end
+
 end
