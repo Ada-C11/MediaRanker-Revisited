fed8b8b71067e63555470a0decdb556cc57271c8
Nara <naraa_520@yahoo.com>
2019-04-29 16:38:48 -0700

    Updates fixtures and ads controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,9 @@
-ENV["RAILS_ENV"] = "test"
-require File.expand_path("../../config/environment", __FILE__)
-require "rails/test_help"
-require "minitest/rails"
-require "minitest/skip_dsl"
-require "minitest/reporters"  # for Colorized output
+ENV['RAILS_ENV'] = 'test'
+require File.expand_path('../config/environment', __dir__)
+require 'rails/test_help'
+require 'minitest/rails'
+require 'minitest/skip_dsl'
+require 'minitest/reporters' # for Colorized output
 
 #  For colorful output!
 Minitest::Reporters.use!(
@@ -23,4 +23,32 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   # Add more helper methods to be used by all tests here...
+  def setup
+    # Once you have enabled test mode, all requests
+    # to OmniAuth will be short circuited to use the mock authentication hash.
+    # A request to /auth/provider will redirect immediately to /auth/provider/callback.
+    OmniAuth.config.test_mode = true
+  end
+
+  # Test helper method to generate a mock auth hash
+  # for fixture data
+  def mock_auth_hash(user)
+    {
+      provider: user.provider,
+      uid: user.uid,
+      info: {
+        email: user.email,
+        username: user.username
+      }
+    }
+  end
+
+  def perform_login(user = nil)
+    user ||= User.first
+
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+
+    user
+  end
 end
