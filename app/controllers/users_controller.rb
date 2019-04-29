a0d9aaf459ee3a4a55d43b264e66fe178b633dfe
Jessica <jessica.homet@gmail.com>
2019-04-29 14:18:54 -0700

    modify flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,21 +14,26 @@ class UsersController < ApplicationController
     user = User.find_by(uid: auth_hash[:uid], provider: "github")
     if user
       # User was found in the database
-      flash[:success] = "Logged in as returning user #{user.name}"
+      flash[:status] = :success
+      flash[:result_text] = "Logged in as returning user #{user.name}"
+      # raise
     else
       # User doesn't match anything in the DB
       # Attempt to create a new user
       user = User.build_from_github(auth_hash)
 
       if user.save
-        flash[:success] = "Logged in as new user #{user.name}"
+        flash[:status] = :success
+        flash[:result_text] = "Logged in as new user #{user.name}"
       else
         # Couldn't save the user for some reason. If we
         # hit this it probably means there's a bug with the
         # way we've configured GitHub. Our strategy will
         # be to display error messages to make future
         # debugging easier.
-        flash[:error] = "Could not create new user account: #{user.errors.messages}"
+        flash[:status] = :error # or failure?
+        flash[:result_text] = "Could not create new user account: #{user.errors.messages}"
+        flash[:messages] = @work.errors.messages
         return redirect_to root_path
       end
     end
@@ -36,6 +41,13 @@ class UsersController < ApplicationController
     # If we get here, we have a valid user instance
     session[:user_id] = user.id
     return redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
   end
 
   # def login_form
