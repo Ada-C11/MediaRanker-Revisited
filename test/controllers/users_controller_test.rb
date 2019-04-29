ba97e54af52b1df44c00d96e5b2e515551d84bc8
Jessica <jessica.homet@gmail.com>
2019-04-29 16:03:50 -0700

    add tests around logging out

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,18 +2,6 @@ require "test_helper"
 
 describe UsersController do
   describe "auth_callback" do
-    it "logs in an existing user" do
-      start_count = User.count
-      user = users(:grace)
-
-      perform_login(user)
-      must_redirect_to root_path
-      session[:user_id].must_equal user.id
-
-      # Should *not* have created a new user
-      User.count.must_equal start_count
-    end
-
     it "creates a new user" do
       start_count = User.count
       user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
@@ -30,7 +18,36 @@ describe UsersController do
       session[:user_id].must_equal User.last.id
     end
 
+    it "logs in an existing user" do
+      start_count = User.count
+      user = users(:grace)
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+
+      # Should *not* have created a new user
+      User.count.must_equal start_count
+    end
+
     it "redirects to the login route if given invalid user data" do
+    end
+
+    it "logs out a user" do
+      start_count = User.count
+      user = users(:grace)
+
+      perform_login(user)
+
+      delete logout_path
+
+      must_redirect_to root_path
+
+      # Should have created a new user
+      User.count.must_equal start_count
+
+      # The new user's ID should be set in the session
+      session[:user_id].must_equal nil
     end
   end
 end
