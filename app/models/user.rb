9d24c773292b34e6d141dbd576bda7718562173b
qqdipps <sdipp007@gmail.com>
2019-04-29 14:27:46 -0700

    added class custom method to user model build from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   has_many :ranked_works, through: :votes, source: :work
 
   validates :username, uniqueness: true, presence: true
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash["uid"]
+    user.name = auth_hash["name"]
+    user.email = auth_hash["email"]
+    user.provider = auth_hash["provider"]
+    return user
+  end
 end
