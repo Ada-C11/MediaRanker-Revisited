36b599529913c6fcb2ab755a166d41bfc6a5dcfb
Amy Wyatt <amy.wyatt12@outlook.com>
2019-04-29 15:11:43 -0700

    fixed logout bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     user = User.new
     user.uid = auth_hash[:uid]
     user.provider = "github"
-    user.nickname = auth_hash["info"]["nickname"]
+    user.username = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
 
     return user
