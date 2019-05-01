c9232b001296afa3b3e80c3e91155eea70230228
Karla Guadron <ksguadron@gmail.com>
2019-05-01 09:46:59 -0700

    Updated user schema to include uid, provider, name, and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,37 @@ class UsersController < ApplicationController
 
   def create
     auth_hash = request.env["omniauth.auth"]
-    raise
+
+    user = User.find_by(uid: auth_hash[:uid], provider: "github")
+    if user
+      # User was found in the database
+      flash[:status] = :success
+      flash[:message] = "Logged in as returning user #{user.name}"
+    else
+      # User doesn't match anything in the DB
+      # Attempt to create a new user
+      user = User.build_from_github(auth_hash)
+
+      if user.save
+        flash[:status] = :success
+        flash[:message] = "Logged in as new user #{user.name}"
+      else
+        # Couldn't save the user for some reason
+        flash[:status] = :error
+        flash[:message] = "Could not create new user account: #{user.errors.messages}"
+        return redirect_to root_path
+      end
+    end
+
+    session[:user_id] = user.id
+    return redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out!"
+
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
 
-  get "/auth/github", as: "github_login", as: "github_login"
-  get "/auth/:provider/callback", to: "users#create", as: "logout"
+  get "/auth/github", as: "github_login"
+  get "/auth/:provider/callback", to: "users#create"
+  delete "/logout", to: "users#destroy", as: "logout"
 end
