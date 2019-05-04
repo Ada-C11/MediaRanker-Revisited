520e874437b7c0d244196bb356230badc76f53f5
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-05-04 15:20:31 -0700

    Added require_login to application controller:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,24 @@ class ApplicationController < ActionController::Base
     raise ActionController::RoutingError.new("Not Found")
   end
 
-  private
-
   def find_user
-    if session[:user_id]
-      @login_user = User.find_by(id: session[:user_id])
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    current_user = find_user
+
+    if current_user.nil?
+      flash[:error] = "You must log in first."
+      redirect_to root_path
     end
   end
+  
+  # private
+
+  # def find_user
+  #   if session[:user_id]
+  #     @login_user = User.find_by(id: session[:user_id])
+  #   end
+  # end
 end
