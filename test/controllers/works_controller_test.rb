bb53e20aea01db57affc9d1598cae6bc191275c4
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-05-04 17:27:41 -0700

    Added more test for wave 2

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -189,7 +189,21 @@ describe WorksController do
 
   describe "upvote" do
     it "redirects to the work page if no user is logged in" do
-      skip
+      # skip
+      work = works(:poodr)
+      get work_path(work.id)
+
+      user = users(:dan)
+      delete logout_path
+      expect(session[:user_id]).must_be_nil
+
+      expect{ 
+        post upvote_path(work.id) 
+      }.wont_change "Vote.count"
+
+      must_respond_with :redirect
+      must_redirect_to work_path(work.id)
+
     end
 
     it "redirects to the work page after the user has logged out" do
