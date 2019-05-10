319d5c0dec7397da5779ce55a23cbeaf2dcd67bb
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-10 14:20:00 -0700

    Got back to passing existing tests

--- app/models/add_name_and_email_to_user.rb
+++
@@ -1,4 +0,0 @@
-class AddNameAndEmailToUser < ApplicationRecord
-  add_column :users, :name, :string
-  add_column :users, :email, :string
-end
--- test/models/add_name_and_email_to_user_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-describe AddNameAndEmailToUser do
-  let(:add_name_and_email_to_user) { AddNameAndEmailToUser.new }
-
-  it "must be valid" do
-    value(add_name_and_email_to_user).must_be :valid?
-  end
-end
