82f14c78e70ccf4768e7fade91545baca8b8ab21
Jansen <v.jansen.martin@gmail.com>
2019-05-11 15:15:51 -0700

    Add test for creating a new user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,8 +2,10 @@ require "test_helper"
 
 describe UsersController do
   describe "auth_callback" do
+    before do
+      @start_count = User.count
+    end
     it "logs in an existing user" do
-      start_count = User.count
       user = users(:dan)
 
       OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
@@ -14,7 +16,27 @@ describe UsersController do
 
       session[:user_id].must_equal user.id
 
-      User.count.must_equal start_count
+      User.count.must_equal @start_count
+    end
+
+    it "creates a new user" do
+      user = User.new(
+        provider: "github",
+        uid: 9999999,
+        username: "fishy",
+      )
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      User.count.must_equal @start_count + 1
+
+      session[:user_id].must_equal User.last.id
+    end
+
+    it "redirects to different route if given invalid user data" do
     end
   end
 end
