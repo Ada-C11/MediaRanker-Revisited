39390c196bfb7ec5e94bc6fe3585bc01bd8bef5a
Kelly Downes <kedownes@gmail.com>
2019-05-11 16:58:08 -0700

    Add tests for users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :name, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
 
   def self.build_from_github(auth_hash)
     user = User.new
@@ -10,7 +10,7 @@ class User < ApplicationRecord
     user.provider = "github"
     user.name = auth_hash["info"]["name"]
     user.email = auth_hash["info"]["email"]
-
+    user.username = auth_hash["info"]["nickname"]
     return user
   end
 end
