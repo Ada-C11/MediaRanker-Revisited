4dd61e24ffafc96e3a1fe429cc1ea83739e06c43
Tatiana Quintana <tatsqui@gmail.com>
2019-05-11 16:28:48 -0700

    first test for checking that a logged in user can login is done

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -32,11 +32,12 @@ class ActiveSupport::TestCase
 
   def mock_auth_hash(user)
     return {
-      provider: user.oauth_provider,
-      uid: user.oauth_uid,
+      provider: user.provider,
+      uid: user.uid,
       info: {
         email: user.email,
-        nickname: user.username
+        nickname: user.name,
+        name: user.username
       }
     }
   end
