7dc174eb3b362c95580065eaf6b8634934084709
Nara <naraa_520@yahoo.com>
2019-05-11 13:35:17 -0700

    Updates to user controller, addsrequire login method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
   before_action :find_user
 
   def render_404
@@ -13,6 +12,14 @@ class ApplicationController < ActionController::Base
   def find_user
     if session[:user_id]
       @login_user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    if find_user.nil?
+      flash[:status] = :failure
+      flash[:result_text] = 'You must be logged in to perform this action'
+      redirect_to root_path
     end
   end
 end
