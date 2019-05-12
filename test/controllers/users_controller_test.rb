6d2aa6980a70420f1bc0a597605482adb2de5e43
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-12 14:58:21 -0700

    added index tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,32 @@
 require "test_helper"
 
 describe UsersController do
+  let(:user) { users(:user1) }
+
+  describe "index" do
+    it "should get index when users exist" do
+      get users_path
+
+      must_respond_with :success
+    end
+    it "should get index when no users exist" do
+      User.all do |user|
+        user.destroy
+      end
+
+      get users_path
+
+      must_respond_with :success
+    end
+  end
+  # describe "create" do
+  #   it "can log in an existing user" do
+
+  #     expect {
+  #       user = perform_login(user)
+  #     }.wont_change "User.count"
+
+  #     expect(session[:user_id]).must_equal user.id
+  #   end
+  # end
 end
