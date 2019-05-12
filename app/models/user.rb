6e4908622b5dd4534150df949e61aa1696ce30b1
Mello-Cello <celloelle@gmail.com>
2019-05-12 10:25:50 -0700

    Implemented OAuth. Changed nav to have a single log in / log out button.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   has_many :votes
   has_many :ranked_works, through: :votes, source: :work
 
-  validates :username, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true #changed this from username to name; can add username later
+
+  def self.build_from_github(auth_hash)
+    user = User.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.name = auth_hash[:info][:nickname]
+    user.email = auth_hash[:info][:email]
+
+    # Note that the user has not been saved.
+    # That is done in the users#create action.
+    return user
+  end
 end
