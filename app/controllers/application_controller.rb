11519e76f95272b9d7ae56257abd952ba93b3629
Shamira <marshallscm@gmail.com>
2019-05-12 17:19:47 -0700

    Fixed error message in application_controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     @user = User.find_by(id: session[:user_id])
 
     if @user.nil?
-      flash[:error] = "You must be logged in to view this page!"
+      flash[:result_text] = "You must be logged in to view this page!"
       redirect_to root_path
     end
   end
