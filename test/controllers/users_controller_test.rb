6f0ac50bc5cd09c6a8efb17aebc52db3eb55188b
Pauline Sauget <paulinevsauget@gmail.com>
2019-05-12 21:10:40 -0700

    Test login for existing user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,20 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth callback" do
+    it "can log in an existing user" do
+      # Arrange
+      user = User.first
+
+      # Act
+      expect {
+        perform_login(user)
+        # get '/auth/github/callback'
+      }.wont_change "User.count"
+
+      # Assert
+      expect(session[:user_id]).must_equal user.id
+      must_redirect_to root_path
+    end
+  end
 end
