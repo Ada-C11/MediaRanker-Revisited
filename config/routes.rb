5f937f73c6db325faaf9c87e59e916425cd55b2d
Leon Tager <ltager@twitter.com>
2019-05-12 19:05:25 -0700

    Wave 1 and 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "works#root"
+  root "works#root", as: 'root'
   # get "/login", to: "users#login_form", as: "login"
   # post "/login", to: "users#login"
   # post "/logout", to: "users#logout", as: "logout"
 
-  get "/auth/github", as: "github_login"
-
-  get "/auth/:provider/callback", to: "users#create"
-
-  delete "/logout", to: "users#destroy", as: "github_logout"
+  get '/auth/github', as: 'github_login'
+  get 'auth/:provider/callback', to: 'users#create', as: 'auth_callback'
+  delete '/logout', to: 'users#destroy', as: 'github_logout'
 
 
   resources :works
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,51 @@
 require "test_helper"
 
 describe UsersController do
+  describe 'auth_callback' do
+    it 'creates an account for a new user and redirects to the root route' do
+      start_count = User.count
+      user = User.create(provider: 'github', uid: 87_872, username: 'test_user', name: 'Test User', email: 'test@user.com')
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      User.count.must_equal start_count + 1
+
+      session[:user_id].must_equal User.last.id
+    end
+
+    it 'logs in an existing user and redirects to the root route' do
+      start_count = User.count
+      user = users(:bob)
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+
+    it 'redirects to the login route if given invalid user data' do
+      start_count = User.count
+      user = User.new(provider: 'github', uid: -8, username: 'test', name: 'Test User', email: 'test@user.com')
+
+      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      get auth_callback_path(:github)
+
+      must_redirect_to root_path
+
+      User.count.must_equal start_count
+    end
+
+    it 'redirects to the login route and deletes the session when the user logs out' do
+      user = users(:bob)
+      perform_login(user)
+      delete logout_path(user)
+      must_redirect_to root_path
+
+      session[:user_id].must_equal nil
+    end
+end
 end
