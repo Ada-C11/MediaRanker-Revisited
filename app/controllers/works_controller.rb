c1c375699811cc43feb63efd883d77bdfc56770a
MyriamWD <myriam.waldend@gmail.com>
2019-05-13 14:59:03 -0700

    added authorization require login

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,9 @@
 class WorksController < ApplicationController
   # We should always be able to tell what category
   # of work we're dealing with
+  # before_action :require_login, only: [:new, :create, :edit, :update, :retired]
+  before_action :require_login, except: [:root, :upvote] #modify upvote
+
   before_action :category_from_work, except: [:root, :index, :new, :create]
 
   def root
