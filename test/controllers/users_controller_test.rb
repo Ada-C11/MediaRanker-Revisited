e78f8f6000454aa81bb9ebb01b4714949b634962
Kirsten Anderson <kirsten@kirstenmunson.com>
2019-05-13 15:33:04 -0700

    first couple tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,16 @@
 require "test_helper"
 
 describe UsersController do
+  describe "index" do
+    it "shows the page" do
+      get users_path
+      must_respond_with :success
+    end
+  end
+
+  describe "logging in/out" do
+    before do
+      @user = User.first
+    end
+  end
 end
