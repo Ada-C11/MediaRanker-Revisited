9c5c26919d5ba6653accd09ea3d74e2df8327418
Nidhi <nidhixpatel@gmail.com>
2019-05-13 16:09:36 -0700

    fixed bug omniauth hash github nickname

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -78,59 +78,37 @@ describe UsersController do
   end
 
   describe "auth_callback" do
-    it "logs in an existing user and redirects to the root route" do
-      # Count the users, to make sure we're not (for example) creating
-      # a new user every time we get a login request
+
+    it "logs in an existing user" do
       start_count = User.count
-
-      # Get a user from the fixtures
       user = users(:grace)
-
-      # Tell OmniAuth to use this user's info when it sees
-      # an auth callback from github
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-      # Send a login request for that user
-      # Note that we're using the named path for the callback, as defined
-      # in the `as:` clause in `config/routes.rb`
-      get auth_callback_path(:github)
-
+    
+      perform_login(user)
       must_redirect_to root_path
-
-      # Since we can read the session, check that the user ID was set as expected
-      session[:user_id].must_equal user.id
-
+      session[:user_id].must_equal  user.id
+    
       # Should *not* have created a new user
       User.count.must_equal start_count
     end
 
-    it "creates an account for a new user and redirects to the root route" do
-      skip
-      start_count = User.count
-
-      # Get a user from the fixtures
-      user = users(:grace)
-
-      # Tell OmniAuth to use this user's info when it sees
-      # an auth callback from github
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-      # Send a login request for that user
-      # Note that we're using the named path for the callback, as defined
-      # in the `as:` clause in `config/routes.rb`
-      get auth_callback_path(:github)
-
-
-      # Since we can read the session, check that the user ID was set as expected
-      session[:user_id].must_equal user.id
-
-      # Should *not* have created a new user
-      User.count.must_equal (start_count + 1)
-      must_redirect_to root_path
-    end
-
     it "redirects to the login route if given invalid user data" do
     end
+  end
+
+  it "creates a new user" do
+    start_count = User.count
+    user = User.new(provider: "github", uid: 99999, username: "nilaiparixitpatel", email: "test@user.com")
+  
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    get auth_callback_path(:github)
+ 
+    must_redirect_to root_path
+ 
+    # Should have created a new user
+    User.count.must_equal start_count + 1
+  
+    # The new user's ID should be set in the session
+    session[:user_id].must_equal User.last.id
   end
 
 end
