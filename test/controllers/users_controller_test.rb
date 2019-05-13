736b78a32dc5aac3fe551bc99726eaef53807fae
Bitaman <b.amani.s@gmail.com>
2019-05-13 16:20:08 -0700

    fixed the test for oath from two weeks ago, all passing

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -16,20 +16,38 @@ describe UsersController do
 
     it "creates an account for a new user and redirects to the root route" do
       start_count = User.count
-      user = User.new(oauth_provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
-      perform_login(user)
+      new_user = User.new(
+        username: "new user",
+        uid: 1234567,
+        email: "new_user@gmail.com",
+        provider: :github,
+      )
+      perform_login(new_user)
 
       must_redirect_to root_path
       User.count.must_equal start_count + 1
     end
 
     it "redirects to the login route if given invalid user data(user can't be saved)" do
+     
+        start_count = User.count
+        invalid_user = User.new(provider: "github", username: " ", email: " ")
+
+        perform_login(invalid_user)
+
+
+        must_redirect_to auth_callback_path(:github)
+
+        session[:user_id].must_be_nil
+
+        User.count.must_equal start_count
+    
     end
   end
 
   it "creates a new user" do
     start_count = User.count
-    user = User.new(oauth_provider: "github", oauth_uid: 99999, username: "test_user", email: "test@user.com")
+    user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
 
     OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
     get auth_callback_path(:github)
