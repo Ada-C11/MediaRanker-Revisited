b49945dd08069c4bff40a9c7f1e81a3455cc9332
Angela <angela.ohh@gmail.com>
2019-05-12 18:16:15 -0700

    passing user#index tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,21 @@
 require "test_helper"
 
 describe UsersController do
+  let (:user_one) { users(:dan) }
+  let (:user_two) { users(:kari) }
+  describe "index" do
+    it "should get index when users exist" do
+      get users_path
+      must_respond_with :success
+    end
+
+    it "should get index when no users exist" do
+      User.all do |user|
+        user.destroy
+      end
+
+      get users_path
+      must_respond_with :success
+    end
+  end
 end
