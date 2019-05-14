61e05bc2cf00148ac601d03dd7131038148d7924
Shubha Rajan <shubharajan94@gmail.com>
2019-05-13 21:53:12 -0700

    added test for invalid login info

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,7 @@ describe UsersController do
 
       user = users(:dan)
 
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+      perform_login(user)
 
       get auth_callback_path(:github)
 
@@ -22,8 +22,7 @@ describe UsersController do
       start_count = User.count
       user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
 
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-      get auth_callback_path(:github)
+      perform_login(user)
 
       must_redirect_to root_path
 
@@ -32,7 +31,17 @@ describe UsersController do
       session[:user_id].must_equal User.last.id
     end
 
-    it "redirects to the login route if given invalid user data" do
+    it "redirects to the root path if given invalid user data" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: nil, username: nil, email: nil)
+
+      perform_login(user)
+
+      must_redirect_to root_path
+
+      User.count.must_equal start_count
+
+      session[:user_id].must_be_nil
     end
   end
 end
