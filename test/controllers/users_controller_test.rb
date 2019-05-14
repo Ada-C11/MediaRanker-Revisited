e01bab0c7851ac23dd8837645b88073921afbd53
Erica Norris <norrise120@gmail.com>
2019-05-13 22:43:43 -0700

    added tests for logging in, updated user test and fixtures accordingly

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,43 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user and redirects to the root route" do
+      start_count = User.count
+
+      user = users(:grace)
+
+      perform_login(user)
+
+      must_redirect_to root_path
+
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+
+    it "creates a new user" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com")
+
+      perform_login(user)
+
+      must_redirect_to root_path
+
+      User.count.must_equal start_count + 1
+
+      session[:user_id].must_equal User.last.id
+    end
+
+    it "logs in an existing user" do
+      start_count = User.count
+      user = users(:grace)
+
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal user.id
+
+      User.count.must_equal start_count
+    end
+  end
 end
