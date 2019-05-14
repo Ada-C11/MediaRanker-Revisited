ee65f17ba043f54be27e371a77f7dc78b1b7abec
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-14 09:33:05 -0700

    user edge case tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -31,8 +31,26 @@ describe UsersController do
       expect(session[:user_id]).must_equal user.id
     end
 
-    it "will redirect back to root with a flash message if not coming from github" do
-      # Skip Auth hash creation
+    it "will respond with a redirect if a user is coming from somewhere other than github" do
+      invalid_user = User.find_by(provider: "facebook")
+
+      expect {
+        perform_login(invalid_user)
+      }.wont_change "User.count"
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it "redirects to the login route if given invalid user data" do
+      invalid_user = User.new(provider: "githib", username: "", uid: 50, email: "")
+
+      expect {
+        perform_login(invalid_user)
+      }.wont_change "User.count"
+
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
   end
 
