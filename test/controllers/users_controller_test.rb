7841e3e44589d079d7a3e9faf8bdb06c7c7b9ba1
qqdipps <sdipp007@gmail.com>
2019-05-13 20:04:30 -0700

    testing around loggin in (users#create) complete, green to go =)

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,10 +3,47 @@ require "test_helper"
 describe UsersController do
   describe "create" do
     it "logs in an existing user and redirects to the root route" do
+      expect { perform_login }.wont_change "User.count"
+
+      expect(session[:user_id]).must_equal users(:dan).id
+      expect(flash[:status]).must_equal :success
+      expect(flash[:result_text]).must_equal "Successfully logged in as existing user #{users(:dan).username}"
+
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
-    it "creates an account for a new user and redirects to the root route" do 
+    
+    it "creates an account for a new user and redirects to the root route" do
+      user = User.new(username: "pennywise1.0",
+                      uid: 7655555,
+                      email: "penny@wise.com",
+                      name: "Penny Wiser",
+                      provider: "github")
+      expect { perform_login(user) }.must_change "User.count", 1
+
+      user = User.find_by(username: "pennywise1.0")
+      expect(session[:user_id]).must_equal user.id
+      expect(flash[:status]).must_equal :success
+      expect(flash[:result_text]).must_equal "Successfully created new user #{user.username} with ID #{user.id}"
+
+      must_respond_with :redirect
+      must_redirect_to root_path
     end
-    it "redirects to the login route if given invalid user data" do 
+
+    it "redirects to the login route if given invalid user data" do
+      user = User.new(username: nil,
+                      uid: 999999999,
+                      email: "penny@wise.com",
+                      name: "Penny Wiser",
+                      provider: "github")
+      expect { perform_login(user) }.must_change "User.count", 0
+
+      expect(session[:user_id]).must_be_nil
+      expect(flash[:status]).must_equal :failure
+      expect(flash[:result_text]).must_equal "Could not log in"
+      expect(flash[:messages]).must_equal :username => ["can't be blank"]
+
+      must_respond_with :bad_request
     end
   end
 end
