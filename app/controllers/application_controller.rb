92950071f9dadb4f986bb9ca99758b25924ee27f
Bitaman <b.amani.s@gmail.com>
2019-05-13 18:14:35 -0700

    added test for upvote and editted tests for show and index for logged in user and no logged in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,13 @@ class ApplicationController < ActionController::Base
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+  def oath_login
+    user = User(session[:user_id]) if session[:user_id]
+    unless user
+      flash[:status] = :failure
+      flash[:result_text] = "you must be logged in to view this page"
+      redirect_to root_path
+    end
+  end
 end
