b25fcffba5a75b31e1b6659969a21b110dc66ed4
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-15 10:53:43 -0700

    attempting travis ci

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,4 +1,45 @@
 require "test_helper"
 
 describe UsersController do
+  describe "auth_callback" do
+    it "logs in an existing user" do
+      start_count = User.count
+      user = users(:grace)
+    
+      perform_login(user)
+      must_redirect_to root_path
+      session[:user_id].must_equal  user.id
+    
+      # Should *not* have created a new user
+      User.count.must_equal start_count
+    end
+
+    it "creates a new user" do
+      start_count = User.count
+      user = User.new(provider: "github", uid: 99999, username: "test_user", email: "test@user.com", name: "name")
+    
+      perform_login(user)
+      must_redirect_to root_path
+    
+      # Should have created a new user
+      User.count.must_equal start_count + 1
+    
+      # The new user's ID should be set in the session
+      session[:user_id].must_equal User.last.id
+    end
+
+    it "logs out a user" do
+      start_count = User.count
+      user = users(:grace)
+    
+      perform_login(user)
+
+      delete logout_path
+
+      must_redirect_to root_path
+
+      session[:user_id].must_equal nil
+      User.count.must_equal start_count
+    end
+  end
 end
