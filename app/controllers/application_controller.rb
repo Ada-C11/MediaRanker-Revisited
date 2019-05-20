b9bfe61e099003014a08b5ada314748dab643b23
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-05-20 10:52:43 -0700

    function to require login for wave 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
       @login_user = User.find_by(id: session[:user_id])
     end
   end
+
+  def require_login
+    user = User.find_by(id: session[:user_id])
+    if user.nil?
+      flash[:error] = "You must be logged in"
+      redirect_to root_path
+    end
+  end
 end
